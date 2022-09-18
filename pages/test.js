59f410e72e0952e6e70fd8810d14b0bc00f3f9d2
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2022-09-18 05:11:11 -0400

    feat: everything
    Co-authored-by: stephen-ics <stephen-ics@users.noreply.github.com>

---
+++ pages/test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProfilePage from '../components/ProfilePage'
+
+const test = () => {
+  return (
+    <div>
+        sss
+    </div>
+  )
+}
+
+export default test
